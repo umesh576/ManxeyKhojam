96f04b37c7e6804d16efbb80d5f81a5fc5996cc7
Umesh Joshi <uj19343@gmail.com>
2025-05-05 01:37:30 +0545

    send otp at mail function is sucessfully done

--- src/controller/forgetPassword.controller.ts
+++ src/controller/forgetPassword.controller.ts
@@ -1,7 +1,7 @@
 import customError from "../middleware/errroHandler.middleware";
 import { Request, Response } from "express";
 import User from "../model/user.model";
-import { sendOtp } from "../utils/sendForgetPin.middleware";
+import { sendOtp } from "../utils/sendForgetPin.utils";
 
 export const forgetPassword = async (req: Request, res: Response) => {
   try {
